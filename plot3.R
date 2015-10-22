70201e99edad492d6002d32f1d219abcf7545729
altonga <altonga@gmail.com>
2015-10-21 17:00:48 -0700

    Completed Plot3

---
+++ plot3.R
@@ -0,0 +1,24 @@
+
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+library(dplyr)
+library(ggplot2)
+NEI_tbl_df <- tbl_df(NEI)
+SCC_tbl_df <- tbl_df(SCC)
+head(NEI_tbl_df)
+NEI_tbl_df <- filter(NEI_tbl_df, fips == "24510")
+
+NEI_by_type <- group_by(NEI_tbl_df, type, year)
+
+NEI_by_type_emissions <- summarize(NEI_by_type, total = round(sum(Emissions, na.rm = TRUE), 3))
+maxy <- ceiling(max(NEI_by_type_emissions$total))
+NEI_by_type_emissions$type = factor(NEI_by_type_emissions$type)
+
+png("plot3.png")
+
+g <- ggplot(data=NEI_by_type_emissions, aes(year, total, color=type)) + 
+     geom_point() + 
+     geom_line(aes(group = type)) +
+     labs(title = "Total Emissions per Year by Type", x ="Year", y = expression("Total Emissions, PM"[2.5]*"(Tons)")) 
+print(g)
+dev.off()
